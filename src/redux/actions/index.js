15277b06c6c64ac8bfb997e3802aeab82e19f399
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-27 14:59:45 -0300

    refact: localstorage setItem removed from action of redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -13,24 +13,7 @@ export const saveScore = (score) => ({
   payload: score,
 });
 
-export const getToken = (token) => ({
-  type: 'GET_TOKEN',
-  payload: token,
-});
-
 export const getAssertions = (assertions) => ({
   type: 'GET_ASSERTIONS',
   payload: assertions,
 });
-
-export const fetchAPIPlayer = () => async (dispatch) => {
-  try {
-    const response = await fetch('https://opentdb.com/api_token.php?command=request');
-    const { token } = await response.json();
-    // console.log(token);
-    dispatch(getToken(token));
-    localStorage.setItem('token', token);
-  } catch (error) {
-    console.log(error);
-  }
-};
