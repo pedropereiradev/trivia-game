5d682f795df57b1dfd16d70c6bc95776cc094559
estenio <angelojuliorafel@gmail.com>
2022-05-27 18:23:22 -0300

    test

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { getRanking } from '../services/ranking';
+
 class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    const ranking = getRanking();
+    console.log(ranking);
+    this.setState({
+      ranking: ranking.sort((p, s) => s.score - p.score),
+    });
+  }
+
   render() {
     const { history } = this.props;
+    const { ranking } = this.state;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <ol>
+          {ranking.length > 0 && (
+            ranking.map((player, index) => (
+              <li key={ index }>
+                <img
+                  src={ player.picture }
+                  alt={ `imagem de perfil de ${ player.name} ` }
+                />
+                <p
+                  data-testid={ `player-name-${ index }` }
+                >
+                  { player.name }
+                </p>
+                <p
+                  data-testid={ `player-score-${ index }` }
+                >
+                  { player.score }
+                </p>
+              </li>
+            ))
+          )}
+        </ol>
         <button
           type="button"
           onClick={ () => history.push('/') }
