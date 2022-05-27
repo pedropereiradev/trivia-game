9890b9b8f735bb95036b1cc701923c066fdb15bf
Geovana Augusta <geovanaag@hotmail.com>
2022-05-26 22:41:00 -0300

    Reqs 9, 11, 15, 16 e 19

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,16 @@
-export const saveUserEmail = (email) => ({
+export const saveUserEmail = (gravatarEmail) => ({
   type: 'CHANGE_EMAIL',
-  payload: email,
+  payload: gravatarEmail,
 });
 
 export const saveUserName = (name) => ({
   type: 'CHANGE_NAME',
   payload: name,
+});
+
+export const saveScore = (score) => ({
+  type: 'CHANGE_SCORE',
+  payload: score,
 });
 
 export const getToken = (token) => ({
@@ -17,7 +22,7 @@ export const fetchAPIPlayer = () => async (dispatch) => {
   try {
     const response = await fetch('https://opentdb.com/api_token.php?command=request');
     const { token } = await response.json();
-    console.log(token);
+    // console.log(token);
     dispatch(getToken(token));
     localStorage.setItem('token', token);
   } catch (error) {
