5274f1135ddd500291bb0a58fd4ca4f31794723e
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-31 17:31:47 -0300

    refact: change actions type to consts and refact clearScore in Login

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { getRanking } from '../services/ranking';
-import { saveScore } from '../redux/actions';
 
 class Ranking extends Component {
   constructor() {
     super();
+
     this.state = {
       ranking: [],
     };
@@ -14,15 +13,15 @@ class Ranking extends Component {
 
   componentDidMount() {
     const ranking = getRanking();
-    console.log(ranking);
+
     this.setState({
       ranking: ranking.sort((a, b) => b.score - a.score),
     });
   }
 
   HandlerClick = () => {
-    const { setUserScore, history } = this.props;
-    setUserScore(0);
+    const { history } = this.props;
+
     history.push('/');
   }
 
@@ -58,21 +57,15 @@ class Ranking extends Component {
           onClick={ this.HandlerClick }
           data-testid="btn-go-home"
         >
-          Play Again
+          Jogar novamente
         </button>
       </div>
     );
   }
 }
-// asdsaddasdsadw
 
 Ranking.propTypes = {
   history: PropTypes.objectOf(PropTypes.shape).isRequired,
-  setUserScore: PropTypes.func.isRequired,
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  setUserScore: (score) => dispatch(saveScore(score)),
-});
-
-export default connect(null, mapDispatchToProps)(Ranking);
+export default Ranking;
